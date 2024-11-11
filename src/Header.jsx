6faf0561567d7f625cfb414a3475e2b52d829436
Md.Rabiul Hasan <70057377+md-rabiul-hasan@users.noreply.github.com>
2024-11-11 17:06:36 +0600

    html to convert react

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,35 +1,61 @@
-import React from 'react';
-import Moon from "./assets/icons/moon.svg";
-import Logo from "./assets/logo.svg";
-import Ring from "./assets/ring.svg";
-import Cart from "./assets/shopping-cart.svg";
+import React, { useState } from 'react';
+import Moon from './assets/icons/moon.svg';
+import Logo from './assets/logo.svg';
+import Ring from './assets/ring.svg';
+import Cart from './assets/shopping-cart.svg';
+import CartDetails from './movie/CartDetails';
 
 export default function Header() {
-  return (
-    <header>
-        <nav className="container flex items-center justify-between space-x-10 py-6">
-            <a href="index.html">
-                <img src={Logo} width="139" height="26" alt="" />
-            </a>
+  const [showCart, setShowCart] = useState(false);
 
-            <ul className="flex items-center space-x-5">
-                <li>
-                    <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                        <img src={Ring} width="24" height="24" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                        <img src={Moon} width="24" height="24" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-                        <img src={Cart} width="24" height="24" alt="" />
-                    </a>
-                </li>
-            </ul>
+  // Close the cart details
+  function handleCartDetailsClose() {
+    setShowCart(false);
+  }
+
+  return (
+    <>
+      {/* Render CartDetails component when showCart is true */}
+      {showCart && <CartDetails handleCartDetailsClose={handleCartDetailsClose} />}
+
+      <header>
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          
+          {/* Logo */}
+          <a href="index.html">
+            <img src={Logo} width="139" height="26" alt="Logo" />
+          </a>
+
+          {/* Navigation Icons */}
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Ring} width="24" height="24" alt="Notifications" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Moon} width="24" height="24" alt="Dark Mode" />
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => setShowCart(true)}
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Cart} width="24" height="24" alt="Cart" />
+              </a>
+            </li>
+          </ul>
         </nav>
-    </header>
-  )
+      </header>
+    </>
+  );
 }
--- src/movie/MovieList.jsx
+++ src/movie/MovieList.jsx
@@ -8,7 +8,7 @@ export default function MovieList() {
         <div className="content">
             <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
                 {movies.map((movie) => (
-                        <MovieCard key={movie.id} movie={movie} />
+                    <MovieCard key={movie.id} movie={movie} />
                 ))}				
             </div>
         </div>
